5c615938c4d3dcf76bc065dad37302410232b878
KOSMOSTARuzb <94840242+KOSMOSTARuzb@users.noreply.github.com>
2026-06-22 10:29:16 +0500

    async fix

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bot = TelegramClient(session='anony', api_id=settings.API_ID, api_hash=settings.
 async def main():
     # Test with a known file host. Most fast hosts support Range Requests.
     test_url = "http://ipv4.download.thinkbroadband.com/5GB.zip"
-    asyncio.run(run_pipeline(bot, test_url, settings.OWNER_ID[0]))
+    await run_pipeline(bot, test_url, settings.OWNER_ID[0])
 
 with bot:
     bot.loop.run_until_complete(main())

e9e955ceb4b5209fd7163804f801bf0001ac9b24
KOSMOSTARuzb <94840242+KOSMOSTARuzb@users.noreply.github.com>
2026-06-23 10:24:49 +0500

    testing

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from telethon import TelegramClient
 
 from config import settings
 from downloader import run_pipeline
+import sys
 
 bot = TelegramClient(session='anony', api_id=settings.API_ID, api_hash=settings.API_HASH).start(bot_token=settings.BOT_TOKEN)
 
 async def main():
-    # Test with a file download, no range support.
-    test_url = "http://aws.kosmostar.us.kg:8085/testfile.bin"
+    # Test with a file download
+    test_url = sys.argv[1] if len(sys.argv)>1 else "http://aws.kosmostar.us.kg:8085/testfile.bin"
     await run_pipeline(bot, test_url, settings.OWNER_ID[0])
 
 with bot:
